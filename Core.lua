c1cec30e769d98bd42fc22a16399e9e699bbccd7
Myrroddin <psvander@gmail.com>
2026-06-17 00:08:34 -0600

    [Retail] Bump ToC for patch 12.0.7

--- Core.lua
+++ Core.lua
@@ -2,12 +2,13 @@ local BetterBags = LibStub("AceAddon-3.0"):GetAddon("BetterBags")
 assert(BetterBags, "BetterBags_Archaeology requires BetterBags")
 
 local categories = BetterBags:GetModule("Categories")
-
-local L = BetterBags:GetModule("Localization")
+local BL = BetterBags:GetModule("Localization")
+local context = BetterBags:GetModule("Context")
+local ctx = context:New("BetterBags_Archaeology")
 
 local _, addon = ...
 
 -- create a category and populate with itemIDs
 for index in pairs(addon.db) do
-	categories:AddItemToCategory(index, L:G(PROFESSIONS_ARCHAEOLOGY))
+	categories:AddItemToCategory(ctx, index, BL:G("PROFESSIONS_ARCHAEOLOGY"))
 end
